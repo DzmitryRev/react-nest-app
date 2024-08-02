b2ae2ed0665a9e36abecf4ef03ed9907ad6b00cf
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2024-08-02 21:57:39 +0300

    fix: enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
+  app.enableCors();
   const config = new DocumentBuilder().setVersion("1.0").build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
